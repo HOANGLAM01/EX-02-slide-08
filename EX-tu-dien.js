ace17e51bf177eb53f48e0e48dc261876a4ae48d
Lam <lnhlam99@gmail.com>
2022-08-18 17:35:38 +0700

    fearture-02-fearture

--- EX-tu-dien.js
+++ EX-tu-dien.js
@@ -1,21 +1,24 @@
+var storageEn = ["banana","apple","orange","grape","jackfruit","mango"];
+var storageVn = ["chuối","táo","cam","nho","mít","xoài"];
+var result = "not found";
 function search() {
     var infor = document.getElementById("user").value;
-    var storageEn = ["banana","apple","orange","grape","jackfruit","mango"];
-    var storageVn = ["chuối","táo","cam","nho","mít","xoài"];
-    var index_eng = 0;
-    var result = "";
 
     for (let i = 0;i < storageEn.length;i++) {
         if (infor === storageEn[i]) {
-            index_eng = i;
-        }
-
-        for (let j = 0; j < storageVn.length;j++) {
-                if (index_eng === j){
-                    result = storageVn[j];
-                }
+            result = storageVn[i];
         }
     }
-
     document.getElementById("result").innerHTML = result;
 }
+function timkiem() {
+    var infor = document.getElementById("user").value;
+
+    for (let i = 0; i < storageVn.length; i++) {
+        if (infor === storageVn[i]) {
+            result = storageEn[i];
+        }
+    }
+    document.getElementById("result").innerHTML = result;
+}
+
